9a82449d901c45abbdfd6539e8ec7938eca960a3
Arpan Saini <arpan.saini@joshtechnologygroup.com>
2024-05-30 16:13:24 +0530

    Add bucketkeyprefix to s3 client

--- classes/local/store/s3/client.php
+++ classes/local/store/s3/client.php
@@ -47,6 +47,7 @@ class client extends object_client_base {
     protected $client;
     protected $bucket;
     private $signingmethod;
+    private $bucketkeyprefix;
 
     public function __construct($config) {
         global $CFG;
@@ -422,9 +423,9 @@ class client extends object_client_base {
             new \lang_string('settings:aws:base_url', 'tool_objectfs'),
             new \lang_string('settings:aws:base_url_help', 'tool_objectfs'), ''));
 
-        // $settings->add(new \admin_setting_configtext('tool_objectfs/key_prefix',
-        //     new \lang_string('settings:aws:key_prefix', 'tool_objectfs'),
-        //     new \lang_string('settings:aws:key_prefix_help', 'tool_objectfs'), ''));
+        $settings->add(new \admin_setting_configtext('tool_objectfs/key_prefix',
+            new \lang_string('settings:aws:key_prefix', 'tool_objectfs'),
+            new \lang_string('settings:aws:key_prefix_help', 'tool_objectfs'), ''));
 
         return $settings;
     }
